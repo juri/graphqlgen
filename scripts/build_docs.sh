4379e1747b536f11e8951d7cf6ba29a6e70e435b
Juri Pakaste <juri@juripakaste.fi>
2019-05-30 13:39:44 +0300

    Add a build_docs script

---
+++ scripts/build_docs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Docs by jazzy
+# https://github.com/realm/jazzy
+# ------------------------------
+
+swift package generate-xcodeproj
+bundler exec jazzy \
+    --clean \
+    --author 'Juri Pakaste' \
+    --author_url 'https://juripakaste.fi' \
+    --github_url 'https://github.com/juri/graphqlgen' \
+    --module 'GraphQLGen' \
+    --source-directory . \
+    --readme 'README.md' \
+    --output docs/
+    exit
